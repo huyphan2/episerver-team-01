035097f8006f7c87e6da25c4a216c125dd1d4e8b
NITECO\huy.nguyen3 <huy.nguyen3@niteco.se>
2021-05-04 10:03:17 +0700

    no message

--- Sources/EPiServer.Reference.Commerce.Site/Features/ProductListing/Services/ProductListingService.cs
+++ Sources/EPiServer.Reference.Commerce.Site/Features/ProductListing/Services/ProductListingService.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using EPiServer.Reference.Commerce.Site.Features.Product.ViewModels;
+using EPiServer.Reference.Commerce.Site.Features.ProductListing.ViewModels;
 
 namespace EPiServer.Reference.Commerce.Site.Features.ProductListing.Services
 {
     public class ProductListingService : IProductListingService
     {
+        public IEnumerable<ProductTileViewModel> GetListProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public FilterParams GetFilterParams()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
