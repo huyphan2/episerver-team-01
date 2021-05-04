b8fad84c063841ba1a1cc18099e4713eb740ddc2
Ratio-Laptop <huynguyen98.clc@gmail.com>
2021-05-04 08:20:09 +0700

    add epi find singleton service

---
+++ Sources/EPiServer.Reference.Commerce.Site/Infrastructure/DependencyResolverInitialization.cs
@@ -0,0 +1,28 @@
+﻿using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.Reference.Commerce.Site.Infrastructure.Epi.Find;
+using EPiServer.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EPiServer.Reference.Commerce.Site.Infrastructure
+{
+    [InitializableModule]
+    public class DependencyResolverInitialization : IConfigurableModule
+    {
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            context.Services.AddTransient<EpiserverFindService>();
+        }
+
+        public void Initialize(InitializationEngine context)
+        {            
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {            
+        }
+    }
+}
---
+++ Sources/EPiServer.Reference.Commerce.Site/Infrastructure/Epi.Find/FilterBase.cs
@@ -0,0 +1,14 @@
+﻿using EPiServer.Find;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPiServer.Reference.Commerce.Site.Infrastructure.Epi.Find
+{
+    public class FilterBase
+    {
+        protected readonly IClient FindClient = EpiserverFind.Instance.Create();
+    }
+}
