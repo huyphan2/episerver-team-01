8dd020deb08f7d66aaa1bcfe194aba744e14d909
NITECO\huy.nguyen3 <huy.nguyen3@niteco.se>
2021-05-06 08:45:04 +0700

    no message

--- Sources/EPiServer.Reference.Commerce.Site/WebApi/ProductListApiController.cs
+++ Sources/EPiServer.Reference.Commerce.Site/WebApi/ProductListApiController.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using EPiServer.Reference.Commerce.Site.Features.Product.ViewModels;
+using EPiServer.Reference.Commerce.Site.Features.ProductListing.Services;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace EPiServer.Reference.Commerce.Site.WebApi
@@ -10,6 +9,21 @@ namespace EPiServer.Reference.Commerce.Site.WebApi
     [RoutePrefix("api/productlist")]
     public class ProductListApiController : BaseApiController
     {
+        private IProductListingService _productListingService;
 
+        public ProductListApiController(IProductListingService productListingService)
+        {
+            _productListingService = productListingService;
+        }
+        public List<ProductTileViewModel> GetProductList(string brand, string category, decimal price,bool sortAlphabet=false)
+        {
+            var result = _productListingService.GetListProduct(brand, price, category);
+            if (result != null)
+            {
+                if (sortAlphabet) result = result.OrderBy(x => x.DisplayName);
+                return result.ToList();
+            }
+            return new List<ProductTileViewModel>();
+        }
     }
 }
