00ebe38f5cc56417f618c7541f0ecd13d2bfe95f
NITECO\huy.nguyen3 <huy.nguyen3@niteco.se>
2021-05-11 15:37:01 +0700

    edit file merge

--- Sources/EPiServer.Reference.Commerce.Site/Infrastructure/BundleConfig.cs
+++ Sources/EPiServer.Reference.Commerce.Site/Infrastructure/BundleConfig.cs
@@ -11,21 +11,26 @@ namespace EPiServer.Reference.Commerce.Site.Infrastructure
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap*"));
             bundles.Add(new ScriptBundle("~/bundles/mustache").Include("~/Scripts/mustache.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/js").Include("~/Scripts/js/*.js"));
+            bundles.Add(new StyleBundle("~/styles/bundled").Include("~/Styles/style.css"));
             //bundles.Add(new StyleBundle("~/styles/bundled").Include("~/Styles/style.css"));
-
+            bundles.Add(new StyleBundle("~/bundles/reset").Include("~/Static/css/reset.css"));
 
             //css
             bundles.Add(new StyleBundle("~/bundles/style")
                 .Include("~/Static/libs/bootstrap.min.css")
                 .Include("~/Static/libs/font-awesome.css")
                 .Include("~/Static/libs/slick.min.css")
-                .Include("~/Static/css/reset.css")
                 .Include("~/Static/css/font.css")
                 .Include("~/Static/css/header.css")
                 .Include("~/Static/css/product-card.css")
+                .Include("~/Static/css/news-static.css")
+                .Include("~/Static/css/shop-category.css")
+                .Include("~/Static/css/product-list-component.css")
                 .Include("~/Static/css/footer.css")
                 .Include("~/Static/css/styles.css")
+                .Include("~/Static/css/product-list-page.css")
                 .Include("~/Static/css/home.css")
+
             );
 
 
